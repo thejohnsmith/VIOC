32cd52d40c95ac91f856d815d12128bf93890476
John Smith <john@thejohnsmith.com>
2016-02-19 12:29:53 -0600

    UI changes and Hover to get program descriptions
    Issue #12 Retrieve programData and programMeta from each program's API
    result and render it.
    Deleted #programManagementTabs input { width: 50% ) - Line 41
    Changed .resp-tab-content { width } to 80% - Line 107
    Updated ico-pdf.png background.
    Changed "Used by 1 Store" to "Used by 3 Stores"

--- static/js/app/getProgramParticipationStats.js
+++ static/js/app/getProgramParticipationStats.js
@@ -3,22 +3,17 @@
  * @param {int} programId, this is taken from the location URL
  * @param {object} data.userId
  */
-
-var getProgramParticipationStats = (function($) {
-
+var getProgramParticipationStats = (function ($) {
   /* Use getHashParams.js to get programId */
   var programId = getHashParams.hashParams.programId;
-  var makeRequest = function() {
+  var makeRequest = function () {
       // Make sure there's a User ID loaded from Marcom before we Init this script.
-      if (marcomUserData.$user.externalId === '%%User.ExternalId%%') {
+      if(marcomUserData.$user.externalId === '%%User.ExternalId%%') {
         return
       }
-      var localDevUrl =
-        'data/getProgramParticipationStats.jssp';
-      var marcomDevUrl =
-        'https://files.marcomcentral.app.pti.com/epsilon/static/data/getProgramParticipationStats.jssp';
-      var acUrl =
-        'https://adobe-uat-vioc.epsilon.com/jssp/vioc/getProgramParticipationStats.jssp';
+      var localDevUrl = 'data/getProgramParticipationStats.jssp';
+      var marcomDevUrl = 'https://files.marcomcentral.app.pti.com/epsilon/static/data/getProgramParticipationStats.jssp';
+      var acUrl = 'https://adobe-uat-vioc.epsilon.com/jssp/vioc/getProgramParticipationStats.jssp';
       $.ajax({
         url: acUrl,
         type: 'GET',
@@ -30,11 +25,11 @@ var getProgramParticipationStats = (function($) {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         }
-      }).done(function(result) {
+      }).done(function (result) {
         //upDateProgramsDashboard(result);
         getProgramTitle(result);
         loadProgramData(result);
-      }).fail(function() {
+      }).fail(function () {
         requestFailed();
       });
     },
@@ -43,23 +38,18 @@ var getProgramParticipationStats = (function($) {
      * @param {object} result Json object from API request.
      * @return {string} programTitle
      */
-    getProgramTitle = function(result) {
-      if (!$('.h1.page-title').length) {
+    getProgramTitle = function (result) {
+      if(!$('.h1.page-title').length) {
         return;
       }
-      return result.map(function(obj) {
+      return result.map(function (obj) {
         var programTitle = getHashParams.hashParams.programId;
-        if (JSON.stringify(obj.id) === programId) {
+        if(JSON.stringify(obj.id) === programId) {
           programTitle = obj.programName;
           return setProgramTitle(programTitle);
         }
-        if (programId === '#' ||
-          programId === 'l' ||
-          programId === '=' ||
-          programId < 1) {
-          console.warn(
-            'Program ID was not found: %c Result is unknown',
-            'color: #f10; font-weight: bold;');
+        if(programId === '#' || programId === 'l' || programId === '=' || programId < 1) {
+          console.warn('Program ID was not found: %c Result is unknown', 'color: #f10; font-weight: bold;');
           programTitle = 'Unknown';
           return setProgramTitle(programTitle);
         }
@@ -71,7 +61,7 @@ var getProgramParticipationStats = (function($) {
      * @param {object} result Json object from API request.
      * @return {string} programTitle found through getProgramTitle.
      */
-    setProgramTitle = function(programTitle) {
+    setProgramTitle = function (programTitle) {
       $('.h1.page-title > *').text(programTitle + ' Program').css({
         opacity: 0
       }).removeClass('hidden').animate({
@@ -79,7 +69,7 @@ var getProgramParticipationStats = (function($) {
       });
       return setBreadcrumbTitle(programTitle);
     },
-    setBreadcrumbTitle = function(programTitle) {
+    setBreadcrumbTitle = function (programTitle) {
       $('#breadcrumbs span').text(programTitle + ' Program');
     },
     /** Load Program Data
@@ -88,16 +78,14 @@ var getProgramParticipationStats = (function($) {
      * @param {string} programDataRendered, html output of $programSelectTpl
      * @retrun {function} upDateProgramsDashboard
      */
-    loadProgramData = function(result) {
-      if ($('.program-select').length) {
-        $.get(
-          'https://files.marcomcentral.app.pti.com/epsilon/static/program-list.mustache.html',
-          function(templates) {
-            var template = $(templates).filter('.program-list-template').html();
-            $('.program-select').html(Mustache.render(template, result));
-            customCheckAndRadioBoxes.customCheckbox();
-            return upDateProgramsDashboard(result);
-          });
+    loadProgramData = function (result) {
+      if($('.program-select').length) {
+        $.get('https://files.marcomcentral.app.pti.com/epsilon/static/program-list.mustache.html', function (templates) {
+          var template = $(templates).filter('.program-list-template').html();
+          $('.program-select').html(Mustache.render(template, result));
+          customCheckAndRadioBoxes.customCheckbox();
+          return upDateProgramsDashboard(result);
+        });
       }
     },
     /**
@@ -106,7 +94,7 @@ var getProgramParticipationStats = (function($) {
      * @param {string} $programId Shorthand for items.
      * @return {string} outputs fill sections.
      */
-    upDateProgramsDashboard = function(result) {
+    upDateProgramsDashboard = function (result) {
       // define colors
       var programStatus = {
         alert: ['status-red'],
@@ -114,20 +102,19 @@ var getProgramParticipationStats = (function($) {
         success: ['status-green'],
         error: ['status-error']
       };
-      return $.each(result, function(index, obj) {
+      return $.each(result, function (index, obj) {
         var $programId = $('#program-' + obj.id);
         // Make sure to ommit non-lifecycle campaign entries.
-        if (obj.isLifecycleCampaign) {
+        if(obj.isLifecycleCampaign) {
           // No stores participating : Red
-          if (obj.storesParticipating === 0 ||
-            obj.storesParticipating === 0 && obj.storesEligible === 0) {
+          if(obj.storesParticipating === 0 || obj.storesParticipating === 0 && obj.storesEligible === 0) {
             $programId.attr('class', programStatus.alert);
           }
           // More than one store participating : Yellow
-          if (obj.storesParticipating > 0 && obj.storesEligible > 0) {
+          if(obj.storesParticipating > 0 && obj.storesEligible > 0) {
             $programId.attr('class', programStatus.warning);
             // All stores participating : Green
-            if (obj.storesParticipating === obj.storesEligible) {
+            if(obj.storesParticipating === obj.storesEligible) {
               $programId.attr('class', programStatus.success);
             }
             updateParticipationDashboard();
@@ -135,18 +122,14 @@ var getProgramParticipationStats = (function($) {
         }
         // Check for errors:
         // If more stores participating than eligible display error state.
-        if (obj.storesParticipating > obj.storesEligible || !$.isNumeric(
-            obj.storesParticipating)) {
+        if(obj.storesParticipating > obj.storesEligible || !$.isNumeric(obj.storesParticipating)) {
           handleDashboardError(obj);
         }
         // Handle errors
         // Color : Gray
         // Displays warning symbol '⚠'
         function handleDashboardError(obj) {
-          return $programId.attr('class', programStatus.error)
-            .find(
-              '.program-status').html('&#9888;').attr('title',
-              'An error occurred');
+          return $programId.attr('class', programStatus.error).find('.program-status').html('&#9888;').attr('title', 'An error occurred');
         }
       });
     },
@@ -154,23 +137,38 @@ var getProgramParticipationStats = (function($) {
      * Updates the participation dashboard
      * @return sets enrolled state to true.
      */
-    updateParticipationDashboard = function() {
-      $('.program-name-lifecycle > [data-enrolled]')
-        .attr('data-enrolled', true);
-      return;
+    updateParticipationDashboard = function () {
+      $('.program-name-lifecycle > [data-enrolled]').attr('data-enrolled', true);
+      getNewProgramDesc();
+      return getFirstProgramDesc();
     },
-    requestFailed = function() {
-      $('.program-select').html(
-        'There was a problem fetching your programs.' +
-        'Please check back again later.'
-      );
-      $('.alert-container').html(
-        '<programss="alert-main alert-danger">DANGER: An error occured</div>'
-      ).fadeIn();
+    /**
+     * Updates the text in .programDesc
+     * @return
+     */
+    getFirstProgramDesc = function () {
+      if($('.program-list li')) {
+        var $firstProgramDesc = $('.program-list li').first().attr('data-programDesc');
+        return setProgramDesc($firstProgramDesc);
+      }
+    },
+    getNewProgramDesc = function () {
+      $('.program-list li').hover(function () {
+        var $newProgramDesc = $(this).attr('data-programDesc');
+        return setProgramDesc($newProgramDesc);
+      }, function () {
+        return;
+      });
+    },
+    setProgramDesc = function (activeProgramDesc) {
+      $('.programDesc').html('<p>' + activeProgramDesc + '</p>');
+    },
+    requestFailed = function () {
+      $('.program-select').html('There was a problem fetching your programs.' + 'Please check back again later.');
+      $('.alert-container').html('<programss="alert-main alert-danger">DANGER: An error occured</div>').fadeIn();
       // Display a success toast, with a title
       console.warn('An internal error has occurred.');
     }
-
   return {
     makeRequest: makeRequest,
     getProgramTitle: getProgramTitle,
@@ -180,8 +178,5 @@ var getProgramParticipationStats = (function($) {
     updateParticipationDashboard: updateParticipationDashboard,
     requestFailed: requestFailed
   };
-
 })(jQuery);
-
-
 getProgramParticipationStats.makeRequest()
