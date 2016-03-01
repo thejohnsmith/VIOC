8929972c31dcfcbb60b35f2eabc6967088c78596
Jeschke <Mark.Jeschke@sf-ltmac17157.corp.epsilon.com>
2016-03-01 09:46:20 -0800

    Added JSON values for the Target Recipients count.

--- static/js/app/getRecipientEstimate.js
+++ static/js/app/getRecipientEstimate.js
@@ -3,6 +3,8 @@ var updateRecipientCount = function()
 	var oilType = $("#oilType").val();
 	var mockStores = [1,2,3];
 	var storeIds = mockStores.join(",");
+	var recipientCount = '';
+	var recipientPlural = 's';
 	
 	$.get( "https://adobe-uat-vioc.epsilon.com/jssp/vioc/getRecipientEstimate.jssp", { 
 		"userId": userId,
@@ -10,7 +12,6 @@ var updateRecipientCount = function()
 		"storeIds": storeIds
 	})
 	  .done(function( data ) {
-	  
 		try 
 		{	
 			data = JSON.parse(data);
@@ -19,10 +20,21 @@ var updateRecipientCount = function()
 		{
 			alert("Failed to parse JSON");
 		}
+		recipientCount = data.recipientCount;
 
-		$("#counter").html("Recipient: " + data.recipientCount);
+		$(".recipient-count").html(recipientCount);
+
+		if (recipientCount > 1) {
+			recipientPlural = 's';
+		} else {
+			recipientPlural = '';
+		}
+		$('.plural').html(recipientPlural);
+		$('.call-to-action .submit').html('Select up to ' + recipientCount + ' Target Recipient' + recipientPlural);
 	  }).error(function(data) {
-		alert("Something bad happened");
+		alert('Cannot access JSON data.');
 	  });
 
 }
+
+updateRecipientCount();
