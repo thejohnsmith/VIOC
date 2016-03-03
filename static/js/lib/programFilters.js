aba86fbe792ba8b13d798370f9f6b11229cd6115
John Smith <john@thejohnsmith.com>
2016-03-03 15:21:57 -0600

    Program Management updates cont. Added a callback in targetingCriteria.js that loads programFilters.js after the API object is available and while I was in there I formatted the file and cleaned some things up. Added Filter updates that were not merged in from a botched branch (feature-program-filter)

--- static/js/lib/programFilters.js
+++ static/js/lib/programFilters.js
@@ -1,5 +1,7 @@
 /** Program Filters
- *
+ * @file programFilters.js
+ * @requires targetingCriteria.jssp
+ * @NOTE ** In order for this script to run it needs to have markup from getStoreSummary.jssp API
  * @param {class} .filters-area Filter container.
  * @param {class} .filter-select.select-option Filter item, contains input.selectbox and .filter-reset.
  * @param {class} .selectbox Input type=select-option.
@@ -10,8 +12,6 @@
  * @param {class} .filter-results-value Span element containing the numarical value of results.
  *
  * ** NOTES **
- * Functions -
- * Calculate TOTAL results
  * Populate first .selectbox options using "filter-type" from getStoreSummary.jssp
  * Show 2ND filter on 1st filter update
  * Show 3RD filter on 2nd filter update
@@ -21,17 +21,19 @@
  * - recalculate TOTAL results
  * - show 2ND filter
  */
+
 var programFilters = new function ($) {
   var $programFiltersComponent = $('.filter-select');
   var $selectOption = $('.select-option');
-  init = function () {
+  var $storeId = $(this).attr('data-storeId');
+  init = function ($storeSummary) {
       if(!$programFiltersComponent.length) {
         return;
       }
-      return handlers();
+      setFilterOptions($storeSummary);
     },
-    handlers = function clickHandlers() {
-      $selectOption.on('change', function (event) {
+    handlers = function () {
+      $('.select-option').on('change', function (event) {
         var $filter = $(this);
         /** Shows the next filter option
          *  Next element is required AND Next element must not visible
@@ -40,7 +42,12 @@ var programFilters = new function ($) {
           var $nextFilter = $filter.next('.filter-select');
           showNextFilter($nextFilter);
         }
+        getStoreCount();
       });
+    },
+    getStoreCount = function () {
+      var $storeCount = $('.program-settings-section .program-settings-store').length
+      return $('.filter-results-value').text($storeCount)
     },
     showFilter = function ($filter) {
       return $filter
@@ -59,10 +66,17 @@ var programFilters = new function ($) {
     showNextFilter = function ($nextFilter) {
       // Get next filter element.
       return showFilter($nextFilter);
+    },
+    setFilterOptions = function ($storeSummary) {
+      var $filter1 = '{{#.}}{{#filter-type}}<div class="{{filter-type}} filter-select select-option">' + '<select class="selectbox">' + '{{#filter-type}}<option class="selectboxOption" selected>Select {{filter-type}}</option>' + '<option class="selectboxOption" value="{{text}}">{{text}}</option>{{/filter-type}}' + '</select>' + '</div>{{/filter-type}}' + '{{#children}}' + '{{#filter-type}}<div class="{{filter-type}} filter-select select-option hidden">' + '<select class="selectbox">' + '{{#filter-type}}<option class="selectboxOption" selected>Select {{filter-type}}</option>{{/filter-type}}' + '{{#children}}<option class="selectboxOption" value="{{text}}">{{text}}</option>{{/children}}' + '</select>' + '</div>{{/filter-type}}' + '{{/children}}' + '{{#children}}' + '<div class="area filter-select select-option hidden">' + '<select class="selectbox">' + '{{#children}}<option class="selectboxOption" selected>Select Area</option>' + '{{#children}}<option class="selectboxOption" value="{{text}}">{{text}}</option>{{/children}}{{/children}}' + '</select>' + '</div>' + '{{/children}}' + '{{/.}}'
+      var filter1html = Mustache.to_html($filter1, $storeSummary);
+      $(filter1html).appendTo('.filters-area-section');
+      return handlers();
     }
   return {
     init: init,
-    hideFilter: hideFilter
+    handlers: handlers,
+    hideFilter: hideFilter,
+    setFilterOptions: setFilterOptions
   };
 }(jQuery);
-programFilters.init();
