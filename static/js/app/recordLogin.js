fbcbcb9b48dca6ef385751d51aae69768c14e9a1
John Smith <john@thejohnsmith.com>
2016-03-07 09:45:39 -0600

    Updated call to recordLogin API. Set Landing Page is now used for different landing page types, depending on results.loginCount value. Changed local mock API data to test.

--- static/js/app/recordLogin.js
+++ static/js/app/recordLogin.js
@@ -7,19 +7,15 @@
      data-user-last-name="%%User.LastName%%"
      data-user-email="%%User.Email%%"
  */
-var recordLogin = (function($) {
-
-  var makeRequest = function() {
+var recordLogin = (function ($) {
+  var makeRequest = function () {
     // Make sure there's a User ID loaded from Marcom before we Init this script.
-    if (marcomUserData.$user.externalId === '%%User.ExternalId%%') {
+    if(marcomUserData.$user.externalId === '%%User.ExternalId%%') {
       return
     }
-    var localDevUrl =
-      'data/recordLogin.jssp';
-    var marcomDevUrl =
-      'https://files.marcomcentral.app.pti.com/epsilon/static/data/recordLogin.jssp';
-    var acUrl =
-      'https://adobe-uat-vioc.epsilon.com/jssp/vioc/recordLogin.jssp';
+    var localDevUrl = 'data/recordLogin.jssp';
+    var marcomDevUrl = 'https://files.marcomcentral.app.pti.com/epsilon/static/data/recordLogin.jssp';
+    var acUrl = 'https://adobe-uat-vioc.epsilon.com/jssp/vioc/recordLogin.jssp';
     $.ajax({
       url: acUrl,
       type: 'GET',
@@ -31,61 +27,29 @@ var recordLogin = (function($) {
       },
       processData: true,
       contentType: 'application/json'
-    }).done(function(data) {
+    }).done(function (data) {
       results = JSON.parse(data);
-      getLoginDays(results);
-    }).fail(function() {
+      if($('#welcome').length) {
+        setLandingPageComponents(results);
+      }
+    }).fail(function () {
       return;
     });
   };
-
-  var getLoginDays = function(results) {
-    var loginDays = {
-      'x': results.firstPortalLogin,
-      'y': results.lastPortalLogin
-    };
-
-    var getDateRange = function(loginDays) {
-        var fistDay = moment(loginDays.x);
-        var lastDay = moment(loginDays.y);
-        var range = lastDay.diff(fistDay, 'days');
-
-        /* Make sure the moment library has loaded */
-        if (typeof moment === 'undefined') {
-          console.log('%c ** Error ** ',
-            'color: #f10; font-weight: bold;',
-            '\nmoment.js was not loaded.');
-          return showGettingStarted();
-        }
-
-        /* Switch sections of home page depending on login times.
-         * If time between last login and first login is greater than 5 days: show Getting Started section.
-         * If time is less then show the Program Summary section.
-         */
-        if (range > 5 && $('.storeProgramData').length) {
-          // Greater than 5, show "Getting Started Now" version of the home page.
-          return showGettingStarted();
-        } else {
-          // Less than 5, show the "Programs" version of the home page.
-          return showPrograms();
-        }
-      },
-      showGettingStarted = function() {
-        $('#welcome + #gettingStartedNow').fadeIn();
-        $('#programSummary').hide();
-      },
-      showPrograms = function() {
-        $('#welcome + #gettingStartedNow').hide();
-        $('#programSummary').fadeIn();
-      }
-
-    return getDateRange(loginDays);
+  var setLandingPageComponents = function (results) {
+    var $loginCount = results.loginCount;
+    if($loginCount > 5) {
+      // Greater than 5, show "Getting Started Now" version of the home page.
+      $('#welcome + #gettingStartedNow').fadeIn();
+      $('#programSummary').hide();
+    } else {
+      // Less than 5, show the "Programs" version of the home page.
+      $('#welcome + #gettingStartedNow').hide();
+      $('#programSummary').fadeIn();
+    }
   }
-
   return {
     makeRequest: makeRequest
   };
 })(jQuery);
-
-
 recordLogin.makeRequest();
