08bd4bef616b3f81304edc3a5352b022c67fe17c
Jeschke <Mark.Jeschke@sf-ltmac17157.corp.epsilon.com>
2016-03-10 10:25:22 -0800

    Added independent CSS and JS files for Marcom catalog fixes

---
+++ static/js/app/overrideCatalogStyles.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+	var containsCatalog = window.location.href.indexOf('catalog.aspx') != -1;
+	$('.ContentBoxGrey').hide();
+	if ($('.ContentBoxGrey').length) {
+		$('.ContentBoxGrey').css('width','1200px');
+		$('.ContentBoxGrey').show();
+	}
+	if (containsCatalog) {
+		$('#catalogContent').css('margin','0 auto');
+	}
+});
