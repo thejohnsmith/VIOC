cc76fdb3a9438fc7c3b5d3542918516960f9c4ed
Jeschke <Mark.Jeschke@sf-ltmac17157.corp.epsilon.com>
2016-03-14 11:14:58 -0700

    Updated CSS & CSS for catalog and shopping cart in their own files.

--- static/js/app/overrideCatalogStyles.js
+++ static/js/app/overrideCatalogStyles.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
 	var containsCatalog = window.location.href.indexOf('catalog.aspx') != -1;
+	var containsAddToCart = window.location.href.indexOf('addToCart.aspx') != -1;
 	$('.ContentBoxGrey').hide();
 	if ($('.ContentBoxGrey').length) {
 		$('.ContentBoxGrey').css('width','1200px');
@@ -7,5 +8,10 @@ jQuery(function ($) {
 	}
 	if (containsCatalog) {
 		$('#catalogContent').css('margin','0 auto');
+	}
+	if (addToCart) {
+		if ($('#CtlBrdCrm').length) {
+			$('#CtlBrdCrm').css('width','1200px');
+		}
 	}
 });
