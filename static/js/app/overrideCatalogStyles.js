fc1f62f33f05cfbf4305c4eafb9f1033b850d359
Jeschke <Mark.Jeschke@sf-ltmac17157.attlocal.net>
2016-03-15 00:19:52 -0700

    One more modification.

--- static/js/app/overrideCatalogStyles.js
+++ static/js/app/overrideCatalogStyles.js
@@ -2,6 +2,7 @@ jQuery(function ($) {
 	var containsCatalog = window.location.href.indexOf('catalog.aspx') != -1;
 	var containsAddToCart = window.location.href.indexOf('addToCart.aspx') != -1;
 	var containsCart = window.location.href.indexOf('cart.aspx') != -1;
+	var containsManageLists = window.location.href.indexOf('manageLists.aspx') != -1;
 	$('.ContentBoxGrey').hide();
 	if ($('.ContentBoxGrey').length) {
 		$('.ContentBoxGrey').css('width','1200px');
@@ -10,7 +11,7 @@ jQuery(function ($) {
 	if (containsCatalog) {
 		$('#catalogContent').css('margin','0 auto');
 	}
-	if (containsAddToCart || containsCart) {
+	if (containsAddToCart || containsCart || containsManageLists) {
 		if ($('#CtlBrdCrm').length) {
 			$('#CtlBrdCrm').css({'width':'1200px','float':'none','margin':'0 auto'}	);
 		}
