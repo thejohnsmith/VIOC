6e25369a0bb62a21493249229a06607fb1866140
John Smith <john@thejohnsmith.com>
2016-04-14 23:18:31 -0500

    A8 - Hide or show the Reports tab based on the user's marcomReportingAccess flag (0 or 1) once A-7 is done.  (Tip: This flag is available from recordLogin.jssp.  Store it in a cookie after you retrieve the results.  https://adobe-uat-vioc.epsilon.com/jssp/vioc/recordLogin.jssp?userId=IQUb4GRsNuHvkPi5%2Bt) B12 - Show the New User home page if loginCount is 5 or less, instead of basing it on login dates; REVISED TO 3

--- static/js/app/recordLogin.js
+++ static/js/app/recordLogin.js
@@ -9,38 +9,64 @@
  */
 var recordLogin = (function ($) {
 	var makeRequest = function () {
-		// Make sure there's a User ID loaded from Marcom before we Init this script.
-		if (marcomUserData.$user.externalId === '%%User.ExternalId%%') {
-			return;
+
+		if (document.cookie.replace(/(?:(?:^|.*;\s*)loginCount\s*\=\s*([^;]*).*$)|^.*$/, "$1") != "")
+		{
+			// We have a cookie containing:
+			var results = {
+				loginCount : document.cookie.replace(/(?:(?:^|.*;\s*)loginCount\s*\=\s*([^;]*).*$)|^.*$/, "$1"),
+				marcomReportingAccess : document.cookie.replace(/(?:(?:^|.*;\s*)marcomReportingAccess\s*\=\s*([^;]*).*$)|^.*$/, "$1")
+			};
+			setLandingPageType(results);
 		}
-		// var localDevUrl = 'data/recordLogin.jssp';
-		// var marcomDevUrl = 'https://files.marcomcentral.app.pti.com/epsilon/static/data/recordLogin.jssp';
-		var apiPath = marcomUserData.$constants.apiPath + 'recordLogin.jssp';
-		$.ajax({
-			url: apiPath,
-			type: 'GET',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			},
-			data: {
-				userId: marcomUserData.$user.externalId
-			},
-			processData: true,
-			contentType: 'application/json'
-		}).done(function (data) {
-			var results = JSON.parse(data);
-			if ($('#welcome').length) {
-				setLandingPageType(results);
-			}
-		}).fail(function () {
-			return;
-		});
+		else
+		{
+			// var localDevUrl = 'data/recordLogin.jssp';
+			// var marcomDevUrl = 'https://files.marcomcentral.app.pti.com/epsilon/static/data/recordLogin.jssp';
+			var apiPath = marcomUserData.$constants.apiPath + 'recordLogin.jssp';
+			$.ajax({
+				url: apiPath,
+				type: 'GET',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+				},
+				data: {
+					userId: marcomUserData.$user.externalId
+				},
+				processData: true,
+				contentType: 'application/json'
+			}).done(function (data) {
+				var results = JSON.parse(data);
+				if ($('#welcome').length) {
+					setLandingPageType(results);
+				}
+			}).fail(function () {
+				return;
+			});
+		}
 	};
 	var setLandingPageType = function (results) {
 		var $loginCount = results.loginCount;
 		var $newUser = getParameterByName('newUser', window.location.href);
 		var $reportingAccess = results.marcomReportingAccess;
-		console.warn('marcomReportingAccess: ' + $reportingAccess);
+
+		document.cookie = 'loginCount=' + $loginCount;
+		document.cookie = 'reportingAccess=' + $reportingAccess;
+
+		/* Show Reports main nav link if reportingAccess is 1 */
+		$('.navBarItem > a').filter(function () {
+			return $(this).text() === 'REPORTS';
+		}).addClass('reports-link');
+
+		if(!$reportingAccess) {
+			console.warn('reportingAccess true');
+		  $('.reports-link').addClass('hide');
+		}
+		if($reportingAccess) {
+			console.warn('reportingAccess false');
+			$('.reports-link').removeClass('hide');
+		}
+
 		if ($newUser > 0) {
 			$loginCount = 1;
 		}
