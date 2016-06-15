cc1b2742d5ac95c89f75289d0fa328f34dc2c57e
John Smith <john@thejohnsmith.com>
2016-06-15 17:24:05 -0500

    Facebook Landing Page Updates Created a BACKUP of the 'before merged' html, index_source_coupons.vioc.com_SEM.html Fixed some meta tag issues in the header, mostly out-date IE hacks and 'best-practice' seven years ago stuff. Moved all JavaScript to bottom of the page! Un-minified the Coupon Form script, later plan is to minify all JS on production. Fixed a couple small alignment issues Added the og:image for Google and Facebook stories. Added more assets: fixed broken image URLs, 64 in total!!! Went through default.css and found every broken image, then tested on coupons.vioc.com to retrieve image, if unsuccessful documentation was left on line in CSS that originally referenced it. Cleaned-up all CSS files using CSSO and CSS-Clean, then beautify. Optimized all image assets except SVG using ImageOptim, then created screen-shots of files/savings/date/author (me). Lots more little things along the way..

--- facebook/scripts/mobile.js
+++ facebook/scripts/mobile.js
@@ -1,56 +1,56 @@
 (function($) {
-    
+
     var $screenMask = $('#mobile-screen-mask');
     var $currentScreen = $('#screen_landing');
-    
+
     $(document).ready(function () {
 
         setMaps();
-        
+
         $('a.to-screen').on('click activate', function() {
-            
+
             if ($(this).hasClass('backwards')) {
-                
-                
+
+
 
                 var $newScreen = $('#screen_' + ($(this).attr('href')).replace('#',''));
                 $currentScreen.addClass('animating-backwards').removeClass('current');
                 $newScreen.addClass('current');
-                
+
                 $currentScreen.animate({
-                    
+
                     left : '0'
-                    
+
                 }, function() {
-                    
+
                     $currentScreen.removeClass('animating-backwards').removeAttr('style');
                     $currentScreen = $newScreen;
-                    
+
                 });
             }
             else {
-                
+
                 var temp = ($(this).attr('href')).replace('#', '');
-                
+
 
                 var $newScreen = $('#screen_' + ($(this).attr('href')).replace('#',''));
                 $newScreen.addClass('animating');
-                
+
                 $newScreen.animate({
-                    
+
                     left : '-50%'
-                    
+
                 }, function() {
-                    
+
                     $currentScreen.removeClass('current');
                     $newScreen.removeClass('animating').addClass('current').removeAttr('style');
                     $currentScreen = $newScreen;
 
                     if (temp == "location") {
 
-                        
+
                     }
-                    
+
                 });
             }
 
@@ -58,12 +58,12 @@
 
                 track('coupon', 'download', $(this).children('.coupon-desc').children('span').children('b').first().text());
             }
-            
+
         });
-        
+
         $('#form_find-location').on('submit', function(e) {
             e.preventDefault();
-            
+
             track('Conversion', 'Location Query', 'Mobile', $(this).children('input[type="text"]').val());
 
             addressSearch($(this).children('input[type="text"]').val(), false, false, function () {
@@ -77,12 +77,12 @@
 
                 }, 50);
             });
-            
+
         });
 
         $('#button_current-location').off('click').on('click', function() {
             if (geo_position_js.init() && !$(this).hasClass('notCurrent')) {
-                
+
                 geo_position_js.getCurrentPosition((function (p) {
                     p.callback = function () {
                         findLocation(Locations[0]);
@@ -104,14 +104,14 @@
 
         });
     });
-    
+
     var findLocation = function(_data) {
         $('#mobile-sem').find('h3.street').text(_data.address.address1);
         $('#mobile-sem').find('h4.city-state').text(_data.address.city + ', ' + _data.address.state);
 
         var d = new Date();
         var hours = "";
-        
+
         switch(d.getDay()) {
 
             case 0:
@@ -139,11 +139,11 @@
 
         if (hours == "Closed")
             $('#mobile-sem').find('.store-hours').text(hours);
-        else 
+        else
             $('#mobile-sem').find('.store-hours').text('Open ' + hours.replace('-', ' - '));
 
         $('#mobile-sem').find('a.directions').attr('href', 'http://maps.google.com?q=' + _data.address.address1 + ' ' + _data.address.city + ' ' + _data.address.state);
         $('#mobile-sem').find('a.call').attr('href', 'tel:'+ _data.address.phone);
     }
-    
+
 })(jQuery);
